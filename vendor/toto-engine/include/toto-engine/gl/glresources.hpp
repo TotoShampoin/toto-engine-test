7ccaf732cd5eb7819d8fe8e5ac121bb7aa352a46
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-09-28 16:37:28 +0200

    Added textures

--- vendor/toto-engine/include/toto-engine/gl/glresources.hpp
+++ vendor/toto-engine/include/toto-engine/gl/glresources.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "glpointer.hpp"
+#include "toto-engine/import-gl.hpp"
 #include <stdexcept>
 #include <string>
 #include <vector>
@@ -13,6 +14,19 @@ enum class GLShaderType {
     TessControl = GL_TESS_CONTROL_SHADER,
     TessEvaluation = GL_TESS_EVALUATION_SHADER,
     Compute = GL_COMPUTE_SHADER
+};
+
+enum class GLTextureTarget {
+    Texture1D = GL_TEXTURE_1D,
+    Texture2D = GL_TEXTURE_2D,
+    Texture3D = GL_TEXTURE_3D,
+    Texture1DArray = GL_TEXTURE_1D_ARRAY,
+    Texture2DArray = GL_TEXTURE_2D_ARRAY,
+    TextureRectangle = GL_TEXTURE_RECTANGLE,
+    TextureCubeMap = GL_TEXTURE_CUBE_MAP,
+    TextureCubeMapArray = GL_TEXTURE_CUBE_MAP_ARRAY,
+    Texture2DMultisample = GL_TEXTURE_2D_MULTISAMPLE,
+    Texture2DMultisampleArray = GL_TEXTURE_2D_MULTISAMPLE_ARRAY
 };
 
 template <GLShaderType SHADER_TYPE>
@@ -64,6 +78,52 @@ public:
     static void unuse() { glUseProgram(0); }
 
     void use() const { use(*this); }
+};
+
+template <GLTextureTarget TARGET, GLsizei N = 1>
+class GLTexture : public GLPointerArray<
+                      [](GLsizei n, GLuint* textures) { glGenTextures(n, textures); },
+                      [](GLsizei n, GLuint* textures) { glDeleteTextures(n, textures); }, N> {
+public:
+    inline static void bind(const GLTexture& texture) { glBindTexture(static_cast<GLenum>(TARGET), texture.handle()); }
+    inline static void unbind() { glBindTexture(static_cast<GLenum>(TARGET), 0); }
+
+    inline void bind() const { bind(*this); }
+
+    void parameter(GLenum parameter, GLint value) const {
+        bind(*this);
+        glTexParameteri(static_cast<GLenum>(TARGET), parameter, value);
+    }
+
+    template <GLenum INTERNAL_FORMAT, GLenum FORMAT, GLenum TYPE>
+    void image2D(GLint level, GLsizei width, GLsizei height, const void* data) const {
+        bind(*this);
+        glTexImage2D(static_cast<GLenum>(TARGET), level, INTERNAL_FORMAT, width, height, 0, FORMAT, TYPE, data);
+    }
+
+    template <GLenum INTERNAL_FORMAT, GLenum FORMAT, typename TYPE>
+    void image2D(GLint level, GLsizei width, GLsizei height, const std::vector<TYPE> data) const {
+        constexpr GLenum type = [&] {
+            if constexpr (std::is_same_v<TYPE, GLubyte>) {
+                return GL_UNSIGNED_BYTE;
+            } else if constexpr (std::is_same_v<TYPE, GLushort>) {
+                return GL_UNSIGNED_SHORT;
+            } else if constexpr (std::is_same_v<TYPE, GLuint>) {
+                return GL_UNSIGNED_INT;
+            } else if constexpr (std::is_same_v<TYPE, GLbyte>) {
+                return GL_BYTE;
+            } else if constexpr (std::is_same_v<TYPE, GLshort>) {
+                return GL_SHORT;
+            } else if constexpr (std::is_same_v<TYPE, GLint>) {
+                return GL_INT;
+            } else if constexpr (std::is_same_v<TYPE, GLfloat>) {
+                return GL_FLOAT;
+            } else {
+                static_assert(false, "Invalid type");
+            }
+        };
+        image2D<INTERNAL_FORMAT, FORMAT, type>(level, width, height, reinterpret_cast<const void*>(data.data()));
+    }
 };
 
 } // namespace toto

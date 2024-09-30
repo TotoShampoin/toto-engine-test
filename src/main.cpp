c87c668f4b997a7ac10b304dd6e2e700a85697ed
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-09-30 09:16:00 +0200

    Moving toto-engine to its own repo - part1

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <functional>
+#include <glm/ext/quaternion_geometric.hpp>
 #include <toto-engine/gl/globjects.hpp>
 #include <toto-engine/gl/glresources.hpp>
 #include <toto-engine/import-gl.hpp>
@@ -14,8 +16,15 @@
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
 #include <imgui.h>
+#include <vector>
 
 using namespace toto;
+
+struct Mesh {
+    Model model;
+    Material material;
+    Transform transform {};
+};
 
 int main(int argc, const char* argv[]) {
     auto window = Window(800, 600, "Hello, World!");
@@ -27,14 +36,27 @@ int main(int argc, const char* argv[]) {
 
     renderer.useProgram();
 
-    auto model = torus(1, .33333333, 96, 32);
-    auto material = Material {
-        .albedo = glm::vec3(1.0f, 0.5f, 0.0f),
-        .metallic = 1.0f,
-        .roughness = 0.25f,
-        .ao = 0.05f,
+    Mesh torus = {
+        shape::torus(1, .33333333, 96, 32),
+        Material {
+                  .albedo = glm::vec3(1.0f, 0.5f, 0.0f),
+                  .metallic = 1.0f,
+                  .roughness = 0.25f,
+                  .ao = 0.05f,
+                  },
     };
-    auto transform = Transform();
+    Mesh cube = {
+        shape::cube(1.0f, 1.0f, 1.0f),
+        Material {
+                  .albedo = glm::vec3(0.5f, 0.5f, 1.0f),
+                  .metallic = 0.0f,
+                  .roughness = 0.5f,
+                  .ao = 1.0f,
+                  },
+    };
+
+    std::vector<std::reference_wrapper<Mesh>> meshes {torus, cube};
+    uint index = 0;
 
     auto camera = Camera::Perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
@@ -42,15 +64,28 @@ int main(int argc, const char* argv[]) {
 
     struct UserData {
         Camera& camera;
-    } data {camera};
+        Renderer& renderer;
+    } data {camera, renderer};
     glfwSetWindowUserPointer(window.handle(), &data);
     glfwSetWindowSizeCallback(window.handle(), [](GLFWwindow* window, int width, int height) {
         auto& data = *static_cast<UserData*>(glfwGetWindowUserPointer(window));
         data.camera.setPerspective(glm::radians(45.0f), width / static_cast<float>(height), 0.1f, 100.0f);
-        glViewport(0, 0, width, height);
+        data.renderer.setCamera(data.camera);
+        data.renderer.setViewport(0, 0, width, height);
     });
 
     renderer.enableDepthTest();
+
+    cube.transform.position() = glm::vec3(-2.0f, 0.0f, 1.0f);
+
+    camera.transform().position() = glm::vec3(4.0f, 3.0f, 4.0f);
+    camera.transform().lookAt(glm::vec3(0.0f));
+
+    light.transform().position() = glm::normalize(glm::vec3(-1.0f, 1.0f, -1.0f));
+    light.transform().lookAt(glm::vec3(0.0f));
+
+    renderer.setCamera(camera);
+    renderer.setLight(light);
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -65,18 +100,14 @@ int main(int argc, const char* argv[]) {
         auto delta = now - last;
 
         auto time2 = time * 0.5f;
-        auto time3 = -time * 0.5f;
 
-        light.transform().rotation() = glm::vec3(0.0f, time2, 0.0f);
-        transform.rotation() = glm::vec3(time3, 0.0f, 0.0f);
-        camera.transform().position() = glm::vec3(4.0f * glm::cos(time), 2.0f, 4.0f * glm::sin(time));
-        camera.transform().lookAt(glm::vec3(0.0f));
-        renderer.setCamera(camera);
-        renderer.setLight(light);
+        torus.transform.rotation() = glm::vec3(0.0f, 0.0f, time);
+        cube.transform.rotation() = glm::vec3(time2);
 
         renderer.clear();
 
-        renderer.render(model, material, transform);
+        renderer.render(torus.model, torus.material, torus.transform);
+        renderer.render(cube.model, cube.material, cube.transform);
 
         //
 
@@ -89,10 +120,16 @@ int main(int argc, const char* argv[]) {
             "Material", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysAutoResize
         );
 
-        ImGui::ColorEdit3("Albedo", &material.albedo.x);
-        ImGui::SliderFloat("Roughness", &material.roughness, 0.0f, 1.0f);
-        ImGui::SliderFloat("Metallic", &material.metallic, 0.0f, 1.0f);
-        ImGui::SliderFloat("AO", &material.ao, 0.0f, 1.0f);
+        ImGui::InputInt("Mesh", reinterpret_cast<int*>(&index));
+        if (index < 0) {
+            index = 0;
+        } else if (index >= meshes.size()) {
+            index = meshes.size() - 1;
+        }
+        ImGui::ColorEdit3("Albedo", &meshes[index].get().material.albedo.x);
+        ImGui::SliderFloat("Roughness", &meshes[index].get().material.roughness, 0.0f, 1.0f);
+        ImGui::SliderFloat("Metallic", &meshes[index].get().material.metallic, 0.0f, 1.0f);
+        ImGui::SliderFloat("AO", &meshes[index].get().material.ao, 0.0f, 1.0f);
 
         ImGui::End();
 
--- vendor/toto-engine/include/toto-engine/import-gl.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// clang-format off
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-// clang-format on
